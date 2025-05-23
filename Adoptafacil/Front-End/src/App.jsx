899281f8ab2ba0c1f8652017ef77a4a3d3c6b9f6
beto18v <nicolasalberto18@hotmail.com>
2025-05-22 21:12:36 -0500

    22/05

--- Adoptafacil/Front-End/src/App.jsx
+++ Adoptafacil/Front-End/src/App.jsx
@@ -1,37 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import DashboardLayout from './layouts/DashboardLayout';
-import Dash from './pages/dashboard/Dash';
-import Error404 from './pages/errores/Error404';
-import Error500 from './pages/errores/Error500';
-import Login from './pages/ingreso/Login';
-import RegisterPage from './pages/registro/Register';
-import TermsAndConditions from './pages/tyc/tyc';
-import StartLayout from './layouts/StartLayout';
-import Main from './pages/inicio/Main.jsx';
-import Perros from './pages/inicio/Perros.jsx';
-import Gatos from './pages/inicio/Gatos';
-import RegistroOpciones from './pages/registro/RegistroOpciones';
-import ConocerMasPerros from './pages/conocer-mas/ConocerMasPerros';
-import ConocerMasGatos from './pages/conocer-mas/ConocerMasGatos';
-import PerfilUsuario from './pages/perfil/PerfilUsuario';
-import DetalleMascota from './pages/mascotas/DetalleMascota';
-import AdminPanel from './pages/admin/AdminPanel';
-import Contacto from './pages/contacto/Contacto';
-import FavoritePets from './pages/dashboard/FavoritePets';
-import Donaciones from './pages/donaciones/Donaciones';
-import PendingRequests from './pages/dashboard/PendingRequests';
-import NotificationsPanel from './pages/dashboard/NotificationsPanel';
-import AdoptionStats from './pages/dashboard/AdoptionStats';
-import AdoptionMap from './pages/dashboard/AdoptionMap';
-import DonationsSummary from './pages/dashboard/DonationsSummary';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Dash from "./pages/dashboard/Dash";
+import Error404 from "./pages/errores/Error404";
+import Error500 from "./pages/errores/Error500";
+import Login from "./pages/ingreso/Login";
+import RegisterPage from "./pages/registro/Register";
+import TermsAndConditions from "./pages/tyc/tyc";
+import StartLayout from "./layouts/StartLayout";
+import Main from "./pages/inicio/Main.jsx";
+import Perros from "./pages/inicio/Perros.jsx";
+import Gatos from "./pages/inicio/Gatos";
+import RegistroOpciones from "./pages/registro/RegistroOpciones";
+import ConocerMasPerros from "./pages/conocer-mas/ConocerMasPerros";
+import PerfilUsuario from "./pages/perfil/PerfilUsuario";
+import DetalleMascota from "./pages/mascotas/DetalleMascota";
+import AdminPanel from "./pages/admin/AdminPanel";
+import Contacto from "./pages/contacto/Contacto";
+import FavoritePets from "./pages/dashboard/FavoritePets";
+import Donaciones from "./pages/donaciones/Donaciones";
+import PendingRequests from "./pages/dashboard/PendingRequests";
+import NotificationsPanel from "./pages/dashboard/NotificationsPanel";
+import AdoptionStats from "./pages/dashboard/AdoptionStats";
+import AdoptionMap from "./pages/dashboard/AdoptionMap";
+import DonationsSummary from "./pages/dashboard/DonationsSummary";
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
-          {/* Ruta padre que usa el layout */}
+          {/* Ruta que usa el layout */}
           <Route path="dashboard" element={<DashboardLayout />}>
             {/* Rutas anidadas para componentes del dashboard */}
             <Route path="Home" element={<Dash />} />
@@ -42,8 +41,8 @@ function App() {
             <Route path="mapa" element={<AdoptionMap />} />
             <Route path="donaciones" element={<DonationsSummary />} />
           </Route>
+          {/* Ruta que usa el Inicio */}
           <Route path="start" element={<StartLayout />}>
-            {/* Ruta hija para la raíz */}
             <Route index element={<Main />} />
             <Route index element={<Perros />} />
             <Route index element={<Gatos />} />
@@ -55,8 +54,10 @@ function App() {
           <Route path="registro-opciones" element={<RegistroOpciones />} />
           <Route path="perros" element={<Perros />} />
           <Route path="gatos" element={<Gatos />} />
-          <Route path="conocer-mas-perros" element={<ConocerMasPerros />} />
-          <Route path="conocer-mas-gatos" element={<ConocerMasGatos />} />
+          <Route
+            path="conocer-mas-perros"
+            element={<ConocerMasPerros />}
+          />{" "}
           <Route path="tyc" element={<TermsAndConditions />} />
           <Route path="mascota/:id" element={<DetalleMascota />} />
           <Route path="admin" element={<AdminPanel />} />
