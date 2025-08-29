cdf07936944aed7a914fc196df5021cedd569f89
beto18v <nicolasalberto18@hotmail.com>
2025-08-29 10:22:43 -0500

    update the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/',
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
